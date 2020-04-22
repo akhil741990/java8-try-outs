3deb1fbf50ff36eaf60585efb7ff78c1d111c646
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2020-04-22 09:31:59 +0530

    Added Java Stream map example

--- src/com/soul/java8/lambda/LambdaExpressionExample.java
+++ src/com/soul/java8/lambda/LambdaExpressionExample.java
@@ -12,3 +12,28 @@ public class LambdaExpressionExample {
 }
 // here we  r assigning lambda exp to a functional interface
 // https://www.infoq.com/articles/Java-8-Lambdas-A-Peek-Under-the-Hood
+
+/*
+ * int total = invoices.stream()
+                    .filter(inv -> inv.getMonth() == Month.JULY)
+                    .mapToInt(Invoice::getAmount)
+                    .sum();
+                    
+                    
+                    
+   int total = invoices.stream()
+                    .filter(new Predicate<Invoice>() {
+                        @Override
+                        public boolean test(Invoice inv) {
+                            return inv.getMonth() == Month.JULY;
+                        }
+                    })
+                    .mapToInt(new ToIntFunction<Invoice>() {
+                        @Override
+                        public int applyAsInt(Invoice inv) {
+                            return inv.getAmount();
+                        }
+                    })
+                    .sum();                 
+ */
+*/
---
+++ src/com/soul/java8/stream/api/StreamExampleFilter.java
@@ -0,0 +1,32 @@
+package com.soul.java8.stream.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class StreamExampleFilter {
+
+	static Map<Integer,String> map = new HashMap<Integer, String>();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<Integer,String> map = new HashMap<Integer, String>();
+		
+		
+		List<Integer> numList = new ArrayList<>();
+		for(int i = 0 ;i < 100 ;i ++) {
+			numList.add(i);
+			map.put(i, "EMP_"+i);
+		}
+		
+		List<String> names = numList.stream().filter(e -> e > 90).map(x -> map.get(x)).collect(Collectors.toList());
+		
+		names.stream().forEach(x -> System.out.println(x));
+	}
+
+	public static String findById(Integer id){
+		return map.get(id);
+	}
+}
