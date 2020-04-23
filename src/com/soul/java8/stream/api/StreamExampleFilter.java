e73386a8bf05c2ac29fc537f0be129c185bd3344
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2020-04-23 11:39:49 +0530

    Added groupBy and partitionBy examples

--- src/com/soul/java8/stream/api/StreamExampleFilter.java
+++ src/com/soul/java8/stream/api/StreamExampleFilter.java
@@ -35,6 +35,43 @@ public class StreamExampleFilter {
 		
 		System.out.println("joining : " + joining);
 		
+		
+		//Partition By
+		
+		Map<Boolean,List<Integer>> partition = numList.stream().collect(Collectors.partitioningBy(i -> i %10 ==0));
+	
+		partition.get(true).stream().forEach(x -> System.out.println(x));
+		
+		// Group By
+		Map<Character, List<String>> group = numList.stream().map(x -> findById(x)).collect(Collectors.groupingBy(x -> new Character(x.charAt(x.length()-1))));
+			
+		group.entrySet().stream().forEach(x -> System.out.println(x.getKey() +" : " + x.getValue() ));
+		
+		Map<Integer, List<Integer>> test = numList.stream().collect(Collectors.groupingBy(x -> x%10));
+	    test.entrySet().stream().forEach(x -> System.out.println(x.getKey() + " : " + x.getValue() ));
+		
+	   //Mapping :  
+	   /*
+	    * Here’s how we can do that; we can use mapping() which can actually adapt the collector to a different type – using a mapping function:
+
+@Test
+public void whenStreamMapping_thenGetMap() {
+    Map<Character, List<Integer>> idGroupedByAlphabet = empList.stream().collect(
+      Collectors.groupingBy(e -> new Character(e.getName().charAt(0)),
+        Collectors.mapping(Employee::getId, Collectors.toList())));
+
+    assertEquals(idGroupedByAlphabet.get('B').get(0), new Integer(2));
+    assertEquals(idGroupedByAlphabet.get('J').get(0), new Integer(1));
+    assertEquals(idGroupedByAlphabet.get('M').get(0), new Integer(3));
+}
+Here mapping() maps the stream element Employee into just the employee id – which is an Integer – using the getId() mapping function. These ids are still grouped based on the initial character of employee first name.
+	    * 
+	    */
+	  
+	   //  https://stackify.com/streams-guide-java-8/
+		
+		
+		
 	}
 
 	public static String findById(Integer id){
