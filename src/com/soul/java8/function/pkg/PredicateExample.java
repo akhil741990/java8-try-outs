371eb6082220fd239f39acc4c0f839a631fa20b4
Akhil Pillai <akhil741990@gmail.com>
2020-10-22 10:35:42 +0530

    Added stream map and filter exmaples

--- src/com/soul/java8/function/pkg/PredicateExample.java
+++ src/com/soul/java8/function/pkg/PredicateExample.java
@@ -1,8 +1,11 @@
 package com.soul.java8.function.pkg;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class PredicateExample {
 
@@ -18,5 +21,15 @@ public class PredicateExample {
 				System.out.println("String doesn't start with 'a'----->" + str);
 			}
 		}
+		//Streams Filter and Collector
+		stringList = stringList.stream().filter(s->s.startsWith("e")).collect(Collectors.toList());
+		System.out.println("String starting with e");
+		stringList.forEach(s-> System.out.println(s));
+		
+		
+		System.out.println("String starting with e in converted to uppercase");
+		//Stream map
+		stringList = stringList.stream().map(s->s.toUpperCase()).collect(Collectors.toList());
+		stringList.forEach(s-> System.out.println(s));
 	}
 }
